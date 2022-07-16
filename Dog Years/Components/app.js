5d468ed9928bb9b3688110520df81db7e069aed1
Mark O'deBeast <80601021+Skatan666@users.noreply.github.com>
2022-07-16 22:06:47 +0200

    Add files via upload

---
+++ Dog Years/Components/app.js
@@ -0,0 +1,23 @@
+//myAge variable is assigned to 24
+let myAge = 24
+
+//earlyYears variable is assigned to 2
+let earlyYears = 2
+
+earlyYears = earlyYears * 10.5
+
+//laterYears value is going to be myAge - 2
+laterYears = myAge - 2
+
+//later years is going to be multiplied by four
+laterYears = laterYears * 4
+
+//to get myAgeInDogYears variable value we have to add the earlyYears and laterYears together
+myAgeInDogYears = earlyYears + laterYears
+
+//assign string name to myName variable
+let myName = 'deBeast'.toLowerCase()
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+ let myAge *= myAge
+
